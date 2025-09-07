8026e49f4578e2bd2180f118275642ffed9ca3f5
Rohit Parit <rohitparit70@gmail.com>
2025-09-07 22:48:41 +0530

    Simple Concepts of ifelse and Oddeven

---
+++ Ifelse.py
@@ -0,0 +1,7 @@
+#This is program to print if User gives the input as 1 then it should print HelloWorld else it should print Bye Bye Kanda
+
+a=int(input("Enter 1 or 0: "))
+if a == 1:
+    print("Hello World")
+else:
+    print("Bye Bye Kanda")  
---
+++ OddEven.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first number: "))
+if a%2==0:
+    print("The Number is Even ")
+else:
+    print("The Number is Odd ")
+    
