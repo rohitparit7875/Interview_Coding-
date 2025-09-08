6b3b441a6520d9bed160bf0a9b6c89e252a05926
Rohit Parit <rohitparit70@gmail.com>
2025-09-08 22:56:15 +0530

    This is Program of Swapping

---
+++ Swap.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("Before swapping: a =", a, ", b =", b)
+
+# Swapping
+temp = a
+a = b
+b = temp
+
+print("After swapping: a =", a, ", b =", b)
