143957ba6d3cc00b836c6655bd81d4af4dd582c7
Rohit Parit <rohitparit70@gmail.com>
2025-09-16 01:06:31 +0530

    simplecacli.py

---
+++ simplecacli.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero."
+
+print("Addition:", add(5, 3))
+print("Subtraction:", subtract(5, 3))
+print("Multiplication:", multiply(5, 3))
+print("Division:", divide(5, 3))
